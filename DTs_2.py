080a1394e59eba8ddee6fee59a14c6f8495bfcff
Todor <tvelichkovt@gmail.com>
2020-04-03 12:41:22 +0300

    files

--- DTs_2.py
+++ DTs_2.py
@@ -1,5 +1,18 @@
 # Load libraries
+import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
 from sklearn.model_selection import train_test_split # Import train_test_split function
 from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
+
+col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
+# load dataset
+pima = pd.read_csv("diabetes.csv", header=None, names=col_names)
+
+pima.describe()
+x= pima.age
+y= pima.bp
+
+plt.plot(x,y)
+plt.show()
+
